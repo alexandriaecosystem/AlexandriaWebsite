600bd9a30eabf00aa48e3cc0068994d1fc1f0b49
Abed Shatila <125126767+Shatila-code@users.noreply.github.com>
2026-08-24 19:36:18 +0300

    Add admin settings route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,6 +12,7 @@ import { UsersPage } from '../pages/UsersPage';
 import { UserConversationPage } from '../pages/UserConversationPage';
 import { MessagesPage } from '../pages/MessagesPage';
 import { TokenMonitorPage } from '../pages/TokenMonitorPage';
+import { AdminSettingsPage } from '../pages/AdminSettingsPage';
 import { ApprovedCommunityPage, KnowledgeGapsPage } from '../pages/AdminOperationsPages';
 import { LanguageProvider } from '../i18n/LanguageContext';
 
@@ -32,6 +33,7 @@ export function App() {
         <Route path="announcements" element={<AnnouncementsPage />} />
         <Route path="analytics" element={<AnalyticsPage />} />
         <Route path="token-monitor" element={<TokenMonitorPage />} />
+        <Route path="settings" element={<AdminSettingsPage />} />
       </Route>
     </Route>
     <Route path="*" element={<Navigate to="/" replace />} />
